fb7726fd1fa923bf0a5ad1ff56be8a5cfce4a1aa
Дарья <basyrova84@inbox.ru>
2025-04-27 20:21:33 +0500

    Add files via upload

---
+++ js/dom.js
@@ -0,0 +1,11 @@
+/*1. Вывод элементов на страницу */
+/*1.1 Выбо по id */
+const h1 = document.getElementById("title");
+console.log(h1);
+/*1.2 Выбор одного элемента по селектору */
+const menu = document.querySelector(".menu");
+console.log(menu);
+/*1.3 Выбор нескольких элементов по селектору */
+const paragraphs = document.querySelectorAll("p");
+console.log(paragraphs);
+
